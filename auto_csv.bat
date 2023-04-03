8dc47858c05659e14f611c35d1a5a26eccbe30c9
DESKTOP-AONS6AJ\kzycc <nagihayatecc@gmail.com>
2023-04-03 16:03:10 +0900

    Update auto_csv.bat

--- auto_csv.bat
+++ auto_csv.bat
@@ -1,3 +1,4 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -File "auto_csv.ps1" -EncodedCommand $(Get-Content "auto_csv.ps1" -Encoding UTF8 | Out-String | ConvertTo-Base64)
+powershell.exe -ExecutionPolicy Bypass -File "auto_csv.ps1" -Encoding utf8
+
 pause
